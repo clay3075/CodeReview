65e5083bba62ef842f09061f4f20f6bc808e0353
Clay Reddick <creddick@techfriends.com>
2018-08-31 17:15:44 -0500

    Added ability to add folders.

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const port = process.env.PORT || 3000;
 const www = process.env.WWW || './www';
 const fs = require('fs');
 const path = require('path');
+var filePaths = "C:\\code\\Reviews";
+
 app.use(express.static(www));
 console.log(`serving ${www}`);
 
@@ -16,7 +18,6 @@ app.listen(port, () => console.log(`listening on http://localhost:${port}`));
 
 //api calls
 app.get('/reviews', (req, res) => {
-  var filePaths = "C:\\code\\Reviews";
   var reviews = []
 
   const isDirectory = source => fs.lstatSync(source.Path).isDirectory();
@@ -25,4 +26,16 @@ app.get('/reviews', (req, res) => {
   reviews = getDirectories(filePaths);
 
   res.send(JSON.stringify(reviews));
+});
+
+app.post('/create/:reviewName', (req, res) => {
+  var dir = path.join(filePaths,req.params.reviewName);
+  console.log(dir);
+  var response = "success";
+  if (!fs.existsSync(dir)){
+      fs.mkdirSync(dir);
+  } else {
+    response = "failure";
+  }
+  res.send(response);
 });
--- www/index.js
+++ www/index.js
@@ -1,11 +1,26 @@
+$( document ).ready(function() {
+  $.get('/reviews', function(data) {
+    reviews = JSON.parse(data);
+    var reviews = new Vue({
+      el: '#reviews',
+      data: {
+        reviews: reviews
+      }
+    });
+  });
 
-$.get('/reviews', function(data) {
-  reviews = JSON.parse(data);
-  var reviews = new Vue({
-    el: '#reviews',
+  var modalVue = new Vue({
+    el: '#modalVue',
     data: {
-      reviews: reviews
+      reviewName: ''
+    },
+    methods: {
+      createNewReview: function () {
+        console.log(this.reviewName);
+        $.post('/create/' + this.reviewName, function(data) {
+          location.reload();
+        });
+      }
     }
-  })
+  });
 });
-
