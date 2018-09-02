d6e2c75f03eee060e3bcf436d2631b989903be34
Clay Reddick <cjreddick96@gmail.com>
2018-09-01 19:02:14 -0500

    Diff viewer sort of works and outline for comments is in place.

--- www/ReviewFiles/reviewfile.js
+++ www/ReviewFiles/reviewfile.js
@@ -1,10 +1,27 @@
-import Vue from "vue";
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    console.log(vars)
+    return vars;
+}
 
 $(document).ready(function() {
-    var diffContent = new Vue({
-        el: '#diffContent',
-        data: {
-            diffHTML: ''
-        }
+    var fileName = getUrlVars()['fileName'];
+    console.log(fileName)
+    $('#pageTitle').html(fileName);
+
+    $.get('/retrievediff/asdf', function(data) {
+        var diffContent = new Vue({
+            el: '#diffContent',
+            computed: {
+                dynamicComponent: function() {
+                    return {
+                        template: '<div>'+data+'</div>'
+                    }
+                }
+            }
+        });
     });
 });
